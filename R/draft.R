37229bf66197bd3c4e9b0aed04d4eab19d03fcdb
Kaspar Meilij <meilikaspar@yahoo.de>
2024-05-24 11:42:05 +0200

    change the overwrite parameter in draft(). If its 'all', evertyhign will be overwritten, if its 'build', just the build directory (useful to update the latex template. Every other value will not overwrite

--- R/draft.R
+++ R/draft.R
@@ -2,11 +2,12 @@
 #'
 #' @param folder
 #'
+#'
 #' @return `TRUE` if all files were copied successfully, `FALSE` otherwise.
 #' @export
 #'
 #' @examples
-draft <- function(folder, overwrite = F){
+draft <- function(folder, overwrite = "none"){
 
 
   # create directories
@@ -14,17 +15,21 @@ draft <- function(folder, overwrite = F){
     folder <- substr(folder,1, nchar(folder)-1)
 
   # check if already exists
-  stopifnot("folder already exists"=!dir.exists(folder) || overwrite)
+  stopifnot("folder already exists"=!dir.exists(folder) || overwrite %in%  c("all", "build" ))
 
+  if(dir.exists(paste0(folder, "/", "build"))){
+    unlink(x = paste0(folder, "/", "build"), recursive =T)
+  }
 
-  dir.create(paste0(folder))
+  dir.create(paste0(folder)) # should fail if already exists
 
   # copy template folder to newly created directory
   template_path <- system.file("./template", package = "pdfRotuce")
 
 
-
-  r = file.copy(from = list.files(template_path, full.names = TRUE), to = folder, recursive = T)
+  # only overwrite if specified. Will still copy build directy as its deleted above
+  ow = overwrite %in% c("all")
+  r = file.copy(from = list.files(template_path, full.names = TRUE), to = folder, recursive = T, overwrite = ow)
 
   # return true if no F from files.copy
   return(all(r))
