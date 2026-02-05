3ddaba41a37b06b51125acaf542a1769f5aa7291
Kaspar Meilij <meilikaspar@yahoo.de>
2026-02-05 19:27:27 +0100

    fix html menu missing references that are not added by PANDOC

--- inst/template_jcsh/build/readthedown/readthedown.js
+++ inst/template_jcsh/build/readthedown/readthedown.js
@@ -1,5 +1,76 @@
 $( document ).ready(function() {
 
+         var $$ = $;
+         var headings = $$('.section');
+         //loop through these and add elements to toc taking into account hierarchy //
+         
+         
+         // code generated with copilot 
+        var rootUL = $$("<ul/>").attr("class", "nav");
+        var listStack = [rootUL];
+        var navLevel = 1;
+
+         headings.each(function() {
+            var a = $$(this); 
+            
+            var classes = a.attr("class").split(/\s+/);
+
+            var currentLevel = classes.find(( el  ) => { return(el.startsWith("level"))});
+            
+            if(currentLevel){
+              currentLevel = currentLevel.match(/\d+/);
+            }
+            
+            
+            if (currentLevel > navLevel) {
+              for (var d = navLevel; d < currentLevel; d++) {
+                var lastLI = listStack[listStack.length - 1].children("li:last");
+                if (!lastLI.length) lastLI = $$("<li/>").appendTo(listStack[listStack.length - 1]);
+                var newUL = $$("<ul/>").appendTo(lastLI);
+                listStack.push(newUL);
+                console.log("going down");
+              }
+            } else if (currentLevel < navLevel) {
+              for (var u = navLevel; u > currentLevel; u--) 
+              listStack.pop();
+              console.log("going up");
+            }
+            navLevel= currentLevel;
+
+            
+            var href = "#".concat(a.attr("id"));
+            
+            var number = a.attr("number");
+        
+            var curHeading = "";
+            
+            var childNodes = a.children("h1:first-of-type, h2:first-of-type, h3:first-of-type, h4:first-of-type, h5:first-of-type, h6:first-of-type").contents().filter(function(){ 
+  return this.nodeType == Node.TEXT_NODE; 
+});
+
+            
+            if(childNodes.length) curHeading = childNodes[0].nodeValue.trim();
+            
+            console.log("current_heading: ".concat(curHeading));
+            
+            var a_id = "toc-".concat(curHeading.replaceAll(" ", "-").toLowerCase());
+            
+            // generate list
+            link = $$("<a>").attr("href", href).attr("id", a_id);
+            span = $$("<span/>").text(number).attr("class", "toc-section-number");
+            //span.appendTo(link);
+            link.append(span).append(" ".concat(curHeading));
+
+            $$("<li/>").append(link).appendTo(listStack[listStack.length - 1]);
+
+            
+ 
+        }) ;
+        
+
+         $$("#toc").empty().append(rootUL); 
+  
+  
     // Fix for dots in level 1 and level 2 titles
     $('body .section.level1').each(function () {
       $(this).attr("id", $(this).attr("id").replace(/\./g, "-"));
